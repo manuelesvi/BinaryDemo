21fc0f353d1ddfc457e709831845060064b1894d
Manuel Espinosa <manuel.espinosa@live.com>
2024-05-19 08:19:27 -0600

    initial commit

---
+++ Digitos.cs
@@ -0,0 +1,15 @@
+﻿public enum Digitos
+{
+    Zero = 0,
+    One = 1,
+    Two = 1 << 1,    // 2
+    Three = 1 << 2, // 4
+    Four = 1 << 3, // 8
+    Five = 1 << 4, // 16
+    Six = 1 << 5, // 32
+    Seven = 1 << 6,//  64
+    Eight = 1 << 7,// 128
+    Nine = 1 << 8,// 256
+    Ten = 1 << 9,// 512
+    Eleven = 1 << 10,// 1024
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine(Imprime(Digitos.Zero));
+Console.WriteLine(Imprime(Digitos.One));
+Console.WriteLine(Imprime(Digitos.Two));
+Console.WriteLine(Imprime(Digitos.Three));
+Console.WriteLine(Imprime(Digitos.Four));
+Console.WriteLine(Imprime(Digitos.Five));
+Console.WriteLine(Imprime(Digitos.Six));
+Console.WriteLine(Imprime(Digitos.Seven));
+Console.WriteLine(Imprime(Digitos.Eight));
+Console.WriteLine(Imprime(Digitos.Nine));
+Console.WriteLine(Imprime(Digitos.Ten));
+Console.WriteLine(Imprime(Digitos.Eleven));
+Console.WriteLine("Press any key to continue...");
+Console.Read();
+
+
+string Imprime(Digitos digitos) => string.Format("decimal: {1}, binario: {0}, octal: {2}, hexadecimal: {3}",
+    Convert.ToString((int)digitos, toBase: 2), (int)digitos,
+    Convert.ToString((int)digitos, toBase: 8),
+    Convert.ToString((int)digitos, toBase: 16));
